802f8520ffca273cd800f8a05480fc2400c9b458
Mateus Behrend <mateusbehrend22@gmail.com>
2024-02-19 12:57:22 -0800

    Added login framework(created username and password fields) + css for login page

--- dischord/src/App.js
+++ dischord/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // create stateVariables
+  // value of the variables will update everytime a keystroke is entired into the box
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DisCHORD</h1>
+      {/*creating login page with username and password fields*/}
+      <div className="login">
+        <label htmlFor="username"> Username </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password"> Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="button" name="Submit" id="Submit" value="SUBMIT" />
+      </div>
     </div>
   );
 }
