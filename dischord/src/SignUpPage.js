4f8b0bea4f2e31ffc983c81cbc8e3ed1c7bb38e8
Mateus Behrend <mateusbehrend22@gmail.com>
2024-03-05 17:08:09 -0800

    Added feature to the signup page where the usernames have to be unique.

--- dischord/src/SignUpPage.js
+++ dischord/src/SignUpPage.js
@@ -8,12 +8,35 @@ const SignUpPage = () => {
     password: "",
     email: "",
   });
+
   //init router hook to change navigation and direction
   const history = useHistory();
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+  //fetch the usernames from the database
+  const fetchUsernames = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/validLogins");
+
+      if (!response.ok) {
+        throw new Error("Bad network response unable to fetch data");
+      }
+
+      const jsonData = await response.json();
+
+      // return the usernames that match in the database
+      const userEntries = jsonData.filter(
+        (item) => item.username === formData.username
+      );
+      return { userEntries };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return { error: error.message };
+    }
+  };
+  // handle the submission of the form
   const handleSub = (e) => {
     const user = { ...formData };
     //ADD: hashing for the password
@@ -34,13 +57,28 @@ const SignUpPage = () => {
       alert("Please enter a password");
       return;
     }
-    //submit form data to JSON database
-    //IMPORANT will need to change fetch address location
-    fetch("http://localhost:8000/validLogins", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-    }).then(() => history.push("/signedUp"));
+    //make process await the return of the existing usernames
+    //check if the username already exists
+    // if it does, alert the user and return
+    // if the username is not already taken then the array will be empty
+
+    fetchUsernames().then((existingUsernames) => {
+      //check if the username already exists
+      // if it does, alert the user and return
+      // if the username is not already taken then the array will be empty
+      if (existingUsernames.userEntries.length > 0) {
+        alert("Username already exists, choose another username");
+        return;
+      }
+
+      //submit form data to JSON database
+      //IMPORTANT will need to change fetch address location
+      fetch("http://localhost:8000/validLogins", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      }).then(() => history.push("/signedUp"));
+    });
   };
   return (
     <>
