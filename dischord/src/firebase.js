6fbdd50db22518fa4d9ca373ce03d668cef2bbc8
Mateus Behrend <mateusbehrend22@gmail.com>
2024-03-12 15:02:05 -0700

    Import firebase api key to firebase.js

--- dischord/src/firebase.js
+++ dischord/src/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 // correct API key: AIzaSyAf-DA_qokjI6KThbHxob9C5AU0ZHM3oLU
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAf-DA_qokjI6KThbHxob9C5AU0ZHM3oLU",
-  //authDomain: "dischord-f6bdb.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "dischord-f6bdb.firebaseapp.com",
   projectId: "dischord-f6bdb",
   //storageBucket: "dischord-f6bdb.appspot.com",
   //messagingSenderId: "272277364746",
   appId: "1:272277364746:web:00664154f844aac9c37ff3",
   //measurementId: "G-NVSN23EZ02"
-}
+};
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
