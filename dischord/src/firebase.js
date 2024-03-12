d9ac7931e92af9be2563e399705bd362d98d8773
arkasharma <arkasharma2004@gmail.com>
2024-03-12 14:54:08 -0700

    resolved merge conflicts

--- dischord/src/firebase.js
+++ dischord/src/firebase.js
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+// correct API key: AIzaSyAf-DA_qokjI6KThbHxob9C5AU0ZHM3oLU
 
 const firebaseConfig = {
   apiKey: "AIzaSyAf-DA_qokjI6KThbHxob9C5AU0ZHM3oLU",
-  authDomain: "dischord-f6bdb.firebaseapp.com",
+  //authDomain: "dischord-f6bdb.firebaseapp.com",
   projectId: "dischord-f6bdb",
-  storageBucket: "dischord-f6bdb.appspot.com",
-  messagingSenderId: "272277364746",
+  //storageBucket: "dischord-f6bdb.appspot.com",
+  //messagingSenderId: "272277364746",
   appId: "1:272277364746:web:00664154f844aac9c37ff3",
-  measurementId: "G-NVSN23EZ02"
-};
+  //measurementId: "G-NVSN23EZ02"
+}
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
